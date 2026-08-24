77c3faf19278147b5e191de03300a27f362a5337
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-24 17:49:10 +0700

    Rename menu item 'Post Artikel' to 'Detail Artikel'

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,7 +3,7 @@ const menuData = [
   { id: 2, title: "Resume", path: "/resume" },
   { id: 3, title: "Proyek", path: "/works" },
   { id: 4, title: "Artikel", path: "/blog" },
-  { id: 5, title: "Post Artikel", path: "/blog_post" },
+  { id: 5, title: "Detail Artikel", path: "/blog_post" },
   { id: 6, title: "Kontak", path: "/contact" },
 ];
 
