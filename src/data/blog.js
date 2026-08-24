3d7d54c4439cf379e352313429ad12f1e66d2cab
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-24 16:26:02 +0700

    Add new blog entry on pressure concepts

--- src/data/blog.js
+++ src/data/blog.js
@@ -4,6 +4,7 @@ const posts = [
   { slug: "peran-media-pembelajaran-digital-dalam-pendidikan", title: "Peran Media Pembelajaran Digital dalam Pendidikan", date: "05 August 2020", image: "/images/blog/blog_5.jpg", category: "Desain", excerpt: "Persiapan olimpiade membutuhkan trik khusus. Berikut adalah rekomendasi buku dan pola latihan soal yang terbukti meloloskan siswa ke tingkat nasional." },
   { slug: "penerapan-metode-stem-kurikulum-merdeka", title: "Penerapan Metode STEM dalam Pembelajaran Fisika", date: "15 Juli 2025", image: "/images/blog/blog_3.jpg", category: "Inspirasi", excerpt: "Bagaimana mengintegrasikan sains, teknologi, teknik, dan matematika di kelas? Simak panduan lengkap menyusun modul ajar berbasis proyek nyata." },
   { slug: "konsep-relativitas-einstein-bahasa-sederhana", title: "Menjelaskan Konsep Relativitas Einstein dengan Bahasa Sederhana", date: "28 Maret 2025", image: "/images/blog/blog_2.jpg", category: "Inspirasi", excerpt: "Teori relativitas sering dianggap rumit dan membingungkan. Artikel ini akan mengupas tuntas paradox kembar dan ruang-waktu secara kasual." },
+  { slug: "konsep-tekanan-dalam-kehidupan-manusia", title: "Menjelaskan Konsep Relativitas Einstein dengan Bahasa Sederhana", date: "12 Agustus 2026", image: "/images/blog/blog_4.jpg", category: "Video", excerpt: "Teori tekanan jika kita meresapi dengan kesungguhan. Artikel ini akan mengupas tuntas bagaimana konsep tekanan sangat berguna untuk kehidupan." },
 ];
 
 export const categories = [
